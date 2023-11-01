78436c447e15033d0c775466afab92307acffe55
ChinjuJose <73712484+ChinjuJose@users.noreply.github.com>
2023-11-01 17:20:23 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("Good Morning!")
+
+var colour = "Red"
+console.log(colour)
+
+console.log("My favourite colour is "+colour)
+
+var x = 10
+var y = 5
+console.log(x+y)
+
+var word1 = "hello"
+var word2 = "WORLD"
+console.log(word1.toUpperCase())
+console.log(word2.toLowerCase())
+
+var sentence = "I am hungry"
+console.log(sentence.replace("hungry","thirsty"))
+
+
+var fruits = ["Apple", "Orange", "Banana"]
+console.log(fruits)
+console.log(fruits[0])
+
+
+
